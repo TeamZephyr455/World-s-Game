8e3cf8b62022d9181fecc6b769e395e5f994cc19
TeamZephyr455 <32281905+TeamZephyr455@users.noreply.github.com>
2017-10-31 11:24:48 -0700

    Level 1 Scripts, Animations, Shooting, Level Design V1.0
    Created Scripts for level 1, input animations, gave a placeholder bullet for main character, Prepped to place in Enemies, and Minor Code for Respawn(Not Implemented yet)

---
+++ Worlds/Assets/Scripts/Lv1 Scripts/Character_Lv1.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+
+public abstract class Character_Lv1 : MonoBehaviour
+{
+    protected Animator myAnimator;
+
+    [SerializeField]
+    protected Transform FirePos;
+
+    [SerializeField]
+    protected float movespeed;
+
+    protected bool faceright;
+
+    public bool Attack { get; set; }
+
+    [SerializeField]
+    protected GameObject FireBulletPrefab;
+
+    // Use this for initialization
+    public virtual void Start ()
+    {
+        faceright = true;
+        myAnimator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeDirection()
+    {
+        faceright = !faceright;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y * 1, transform.localScale.z *1);
+    }
+
+    public virtual void ShootFire(int value)
+    {
+        if (faceright)
+        {
+            GameObject tmp = (GameObject)Instantiate(FireBulletPrefab, transform.position, Quaternion.identity);
+            tmp.GetComponent<FireBullet>().Initialize(Vector2.right);
+        }
+        else
+        {
+            GameObject tmp = (GameObject)Instantiate(FireBulletPrefab, transform.position, Quaternion.Euler(new Vector3(0, -180, 0)));
+            tmp.GetComponent<FireBullet>().Initialize(Vector2.left);
+        }
+    }
+
+}
