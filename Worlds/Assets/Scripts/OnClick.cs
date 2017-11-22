6683f691e992c751383f24695058e241905af76e
TeamZephyr455 <32281905+TeamZephyr455@users.noreply.github.com>
2017-11-21 19:07:19 -0800

    Start Menu has working buttons
    Start Games loads lv3. Quit terminates the standalone application. Note: Running the game in the Unity Editor is not a standalone application.

---
+++ Worlds/Assets/Scripts/OnClick.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//This Script attaches to buttons and gives them common functions
+public class OnClick : MonoBehaviour {
+
+
+    public void LoadNewScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+}
